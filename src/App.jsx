d182cf65df695adad061dea0f44c75b522106649
Vikas24Chauhan <vikas24chauhan@gmail.com>
2026-03-07 21:41:58 +0530

    vercel issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -9,23 +9,29 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
   return (
-    <Router basename="MyPortfolio">
+    <Router>
       <Navbar />
+
       <section id="about">
         <About />
       </section>
+
       <section id="skills">
         <Skills />
       </section>
+
       <section id="experiences">
         <Experiences />
       </section>
+
       <section id="projects">
         <Projects />
       </section>
+
       <section id="education">
         <Education />
       </section>
+
       <Footer />
     </Router>
   );

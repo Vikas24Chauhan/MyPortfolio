a296e5177efbdd3f68d0da0ebde69317367fe018
Vikas24Chauhan <vikas24chauhan@gmail.com>
2026-04-22 23:38:08 +0530

    navbar only

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Navbar } from "./Components/navbar/Navbar";
-import Hero from "./components/hero/Hero";
-import About from "./components/about/About";
-import { Skills } from "./Components/skills/Skills";
-import { Experiences } from "./Components/experiences/Experiences";
-import Projects from "./Components/projects/Projects";
-import Education from "./Components/education/Education";
-import Footer from "./Components/footer/Footer";
+
 import { BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
       <Navbar />
-
-      <Hero />
-
-      <section id="about">
-        <About />
-      </section>
-
-      <section id="skills">
-        <Skills />
-      </section>
-
-      <section id="experiences">
-        <Experiences />
-      </section>
-
-      <section id="projects">
-        <Projects />
-      </section>
-
-      <section id="education">
-        <Education />
-      </section>
-
-      <Footer />
     </Router>
   );
 };

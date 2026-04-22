63ed6989b65bd3e429c1b0e352a2b5706b1cf6c0
Vikas24Chauhan <vikas24chauhan@gmail.com>
2026-04-22 23:24:33 +0530

    navbar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from "react";
+import "./App.css";
 import { Navbar } from "./Components/navbar/Navbar";
 import { About } from "./Components/about/About";
 import { Skills } from "./Components/skills/Skills";

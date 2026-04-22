54d30cfee8b2d1cafc97063f2aa85382f8743e34
Vikas24Chauhan <vikas24chauhan@gmail.com>
2026-04-22 23:44:17 +0530

    new 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Navbar } from "./components/navbar/Navbar";
+// import { Navbar } from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import About from "./components/about/About";
 import { Skills } from "./components/skills/Skills";
@@ -13,7 +13,7 @@ import Footer from "./components/footer/Footer";
 function App() {
   return (
     <Router>
-      <Navbar />
+      {/* <Navbar /> */}
 
       <Hero />
 

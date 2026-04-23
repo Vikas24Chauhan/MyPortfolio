7ceff7ce488e18e04a22f5e0dc5938a654904449
Vikas24Chauhan <vikas24chauhan@gmail.com>
2026-04-23 22:33:22 +0530

    projects updated

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,16 +4,6 @@ import { data, NavLink } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import CDN from "../../assets/images/cdn.png";
-import TenHR from "../../assets/images/ten-hr-consulting.png";
-import TenAI from "../../assets/images/ten-ai-consulting-Labs.png";
-import PublicAdvocacy from "../../assets/images/public_advocacy.png";
-import Edusity from "../../assets/images/edusity.png";
-import Pokemon from "../../assets/images/pokemon.png";
-import TenFunding from "../../assets/images/ten_funding.png";
-import TenToddlers from "../../assets/images/ten_toddlers.png";
-import Imagify from "../../assets/images/imagify.png";
-import VcIncubation from "../../assets/images/vcIncubation.png";
 
 function Projects() {
   const cardsContainerRef = useRef(null);
@@ -49,7 +39,7 @@ function Projects() {
 
         const toScale = 1 - (cards.length - 1 - index) * 0.1;
         const nextCard = cards[index + 1];
-        const cardInner = card.querySelector(".card__inner");
+        const cardInner = card.querySelector(".card-inner");
 
         if (!nextCard || !cardInner) return;
 
@@ -130,62 +120,62 @@ function Projects() {
 
   const projects = [
     {
-      id: 1,
-      title: "TEN HR Consulting",
-      image: TenHR,
+      id: "01",
+      title: "CoreBTR",
+      image:
+        "https://cdn.dribbble.com/userupload/47497062/file/bc876d35285636455e5ae65d60395035.png",
       description:
-        "The TEN HR Consulting website is offering you Entrepreneurship Network’s HR services. The TEN-HR-Consulting website is proud to offer HR services from The Entrepreneurship Network, providing expert solutions to support your business's growth and success.",
-      color: "cyan",
-      link: "https://ten-hr-consulting-self.vercel.app/",
+        "A Unique platform where concepts finally make sense, where revision stops feeling overwhelming, and where preparation becomes clear, structured, and truly doable. Built by Dr. Zainab Vora for students who are tired of confusion and ready for clarity, CoreBTR is designed with one simple goal-learning that is simpler, smarter, and always student-first.",
+      link: "https://corebtr.com",
     },
     {
-      id: 2,
-      title: "TEN Funding",
-      image: TenFunding,
+      id: "02",
+      title: "Believers Consultancy",
+      image:
+        "https://cdn.dribbble.com/userupload/47497391/file/85909bbce7be333d291ab43c03935d8c.png",
       description:
-        "TEN Funding is the ultimate platform for exploring the investors backing The Entrepreneurship Network (TEN). It connects aspiring entrepreneurs with the funding ecosystem that powers TEN's innovative projects. By bridging the gap between visionaries and financial support, TEN Funding fosters a thriving entrepreneurial community.",
-      color: "blue",
-      link: "https://ten-funding-lime.vercel.app/",
+        "Your trusted partner for NEET PG Counselling, INICET, medical college admissions, and career guidance. Expert consultation for medical aspirants with rank predictor and college finder tools.",
+      link: "https://believersconsultancy.com/",
     },
     {
-      id: 3,
-      title: "IMAGIFY",
-      image: Imagify,
+      id: "03",
+      title: "Dr. Zainab Vora",
+      image:
+        "https://cdn.dribbble.com/userupload/47497389/file/0be98ce15ff7ca3c4a9ef74cbeecb771.png",
       description:
-        "Developed a web-based application that converts text into images, allowing users to generate visually appealing text-based graphics. The project utilizes modern web technologies, including React.js for the frontend and Node.js for backend processing. Implemented features such as customizable fonts, colors, and backgrounds to enhance user creativity. Designed an intuitive UI for seamless user experience and optimized performance for fast image generation.",
-      color: "purple",
-      link: "https://imagify-ten-mu.vercel.app/",
+        "Dr. Zainab Vora MD Radiology ( AIIMS Delhi ), National NEET PG Mentor, and the mastermind behind BTR. An AIIMS New Delhi alumnus and AIR 1 in INI-CET, Dr. Zainab Vora is more than an educator - she is a pillar of hope for PG aspirants across India. With compassion and clarity, she has transformed countless journeys through the BTR course - turning confusion into confidence and dreams into reality which is why she known as the “BTR Queen,” To her students, she is not just a mentor but a belief that says, 'Your story can still change'. “Kahani tumhaari hai. Ho sake toh kamaal likhna.”",
+      link: "https://drzainabvora.com/",
     },
     {
-      id: 4,
-      title: "TEN Toddlers",
-      image: TenToddlers,
+      id: "04",
+      title: "Dr. Apurv Mehra",
+      image:
+        "https://cdn.dribbble.com/userupload/47497388/file/db77ac6244fd2f2c0d725d066d148af5.png",
       description:
-        "This website is a comprehensive resource dedicated to Baby Brain Development, offering valuable guidance to help parents nurture their child's cognitive and intellectual growth. With expert mentors available, parents can receive personalized support and scientifically-backed strategies tailored to their baby's developmental stage. The platform covers a wide range of topics, including sensory development, early learning techniques, memory-building activities, and language skills enhancement.",
-      color: "amber",
-      link: "https://ten-toddlers-mu.vercel.app/",
+        "DR. Apurv Mehra MBBS | MS | MD | DNB (Orthopaedics) | DIP.SCOT - Belgium, Leading Orthopedician & Robotic Joint Replacement Surgeon, Renowned Educator, Founder eConceptual, Co - Founder Cerebellum, Founder Vidya Jeevan Education Centre, Founder Vidya Jeevan Clinic for Orthopedics, Bestselling Author of Medical PG Books & Motivational Speaker",
+      link: "https://drapurv.com/",
     },
     {
-      id: 5,
-      title: "Edusity",
-      image: Edusity,
+      id: "05",
+      title: "eConceptual",
+      image:
+        "https://cdn.dribbble.com/userupload/47497390/file/1d672095a08ca88c4aa7aec2cf812306.png",
       description:
-        "Welcome to our College Website, your go-to resource for comprehensive information about our institution. Explore our academic programs, campus life, admissions process, and more. Whether you're a prospective student or just curious about what we offer, you'll find everything you need to know right here.",
-      color: "emerald",
-      link: "https://vikas24chauhan.github.io/edusity/",
+        "A platform for Medical Postgraduate (PG) residents to enhance their skills under the guidance of experienced teachers. App to Master Your Specialization.",
+      link: "https://econceptual.com/",
     },
   ];
 
   return (
     <div id="projects" className="projects-container">
-      <Helmet>
-        <title>Conceptual Radiology Books - Dr. Zainab Vora</title>
+      {/* <Helmet>
+        <title></title>
 
         <meta
           name="description"
-          content="Conceptual Radiology books by Dr. Zainab Vora help for radiology residents build strong basics, clear imaging concepts, & confident exam oriented clinical thinking."
+          content=""
         />
-      </Helmet>
+      </Helmet> */}
 
       <div className="projects-head">
         <p className="projects-heading">Projects</p>
@@ -198,29 +188,51 @@ function Projects() {
       <div className="projects-cards" ref={cardsContainerRef}>
         {projects.map((card, index) => (
           <div
-            key={card.id}
+            key={index}
             className="projects-card"
             data-index={index}
             ref={(el) => addToCardsRef(el, index)}
           >
-            <div className="projects-card__inner">
-              <div className="projects-card__image-container">
-                <img
-                  className="projects-card__image"
-                  src={card.image}
-                  alt={card.title}
-                />
-              </div>
-              <div className="projects-card__content">
-                <h3 className="projects-card__title">{card.title}</h3>
-                <p className="projects-card__description">{card.description}</p>
+            <div className="projects-card-inner">
+              <div className="project-card-info">
+                <h2>
+                  {card.id}
+                  <span>{card.title}</span>
+                </h2>
+
                 <NavLink
-                  className="projects-card__btn"
+                  className="projects-card-btn"
                   to={card.link}
                   target="_blank"
                 >
-                  Check Sample Pages
+                  Live Project
                 </NavLink>
+              </div>
+
+              <div className="projects-card-content">
+                <p className="projects-card-description">{card.description}</p>
+              </div>
+
+              <div className="projects-card-images">
+                <div className="projects-card-image1">
+                  <img
+                    className="projects-card-image"
+                    src={card.image}
+                    alt={card.title}
+                  />
+                </div>
+                <div className="projects-card-image2">
+                  <img
+                    className="projects-card-image"
+                    src={card.image}
+                    alt={card.title}
+                  />
+                  <img
+                    className="projects-card-image"
+                    src={card.image}
+                    alt={card.title}
+                  />
+                </div>
               </div>
             </div>
           </div>
